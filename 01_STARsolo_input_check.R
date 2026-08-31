af538035d70132bf1a890919fa8d28ce942d49df
dorsashaghaghian-collab <dorsa.shaghaghian@gmail.com>
2026-09-01 01:37:22 +0330

    Create 01_STARsolo_input_check.R

---
+++ 01_STARsolo_input_check.R
@@ -0,0 +1,13 @@
+#########################################################
+# Single Cell RNA-seq CRISPR Perturbation Analysis
+#
+# Step 1:
+# Validation of STARsolo outputs
+#
+# Dataset:
+# GSM6243407
+#
+# Species:
+# Mus musculus
+#
+#########################################################
