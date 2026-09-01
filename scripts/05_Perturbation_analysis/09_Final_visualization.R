698611364d7495083006362c5855f9d4185a57e1
dorsashaghaghian-collab <dorsa.shaghaghian@gmail.com>
2026-09-02 01:27:35 +0330

    Create 09_Final_visualization.R

---
+++ scripts/05_Perturbation_analysis/09_Final_visualization.R
@@ -0,0 +1,17 @@
+############################################################
+# Final Publication-style Visualization
+############################################################
+
+
+# Generates final figures:
+
+# 1. Perturbation UMAP
+# 2. Differential expression volcano plots
+# 3. GSEA NES heatmap
+# 4. AUCell signature heatmap
+# 5. Mechanistic interpretation figure
+
+
+# Output directory:
+
+# ../03_results/Figures/
