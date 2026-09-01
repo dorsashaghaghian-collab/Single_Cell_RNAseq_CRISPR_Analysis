ff249eeb4afe307d90ec678b1ef74521e9019e77
dorsashaghaghian-collab <dorsa.shaghaghian@gmail.com>
2026-09-02 01:26:40 +0330

    Create 07_Pathway_GSEA_analysis.R

---
+++ scripts/05_Perturbation_analysis/07_Pathway_GSEA_analysis.R
@@ -0,0 +1,27 @@
+############################################################
+# Pathway Enrichment and Hallmark GSEA
+############################################################
+
+library(clusterProfiler)
+library(enrichplot)
+
+
+# Input:
+# DESeq2 ranked gene lists
+
+
+# Analysis:
+
+# GO Biological Process enrichment
+# Hallmark pathway GSEA
+
+
+# Generated outputs:
+
+# GSEA_Axl_Hallmark_results.csv
+# GSEA_Prmt1_Hallmark_results.csv
+# GSEA_Ripk1_Hallmark_results.csv
+
+
+# NES comparison matrix generated for
+# perturbation pathway comparison
